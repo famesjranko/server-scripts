bee2434ff53b267d33d96045a714374c5bbb52f5
famesjranko <andrewmcdonald42@gmail.com>
2020-11-27 17:40:13 +1100

    Update temp-report.sh

--- temp-report.sh
+++ temp-report.sh
@@ -3,26 +3,23 @@
 # This is a simple script that prints gpu temp, cpu temp, local temp, and deltas.
 # Can be used in conjunction with cron scheduling to record temps to log.
 
-# REQUIRES: weather-util or curl to collect local weather data, nvidia-smi for 
-# nvidia gpu temp, and sensors for cpu temp.
+# REQUIRES: curl to collect local weather data, nvidia-smi for nvidia gpu temp, 
+# and sensors for cpu temp.
 #===============================================================================
 
-## get local date and temp
+## get local date and time
 date=$(date "+%D - %T")
 
-# weather-util issue when used in cron
-#local_temp=$(weather "ymml" | grep Temperature | awk '{print $4}' | cut -c 2-)
-
-# using wttr.in
+# get local temp from wttr.in
 local_data=$(curl wttr.in/Melbourne?format=1 2> /dev/null)
 local_temp=$(echo $local_data | awk '{print $2}' | cut -c 2-3)
 
-## get hardware temp
+## get hw temp
 cpu_temp=$(sensors | grep 'Package id 0' | cut -c 17-18)
 #cpu_temp=$(/opt/vc/bin/vcgencmd measure_temp | cut -c 6-7) # for raspberry-pi
 gpu_temp=$(nvidia-smi -q -d temperature | grep 'GPU Current Temp' | cut -c 45-46)
 
-## calc hardware/local temp delta
+## calc hw/local temp delta (can assume local temp will always be lower than hw temp)
 cpu_delta=$(($cpu_temp - $local_temp))
 gpu_delta=$(($gpu_temp - $local_temp))
 
