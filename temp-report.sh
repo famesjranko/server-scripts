4a22b045bb1b0699af562a204ab10a5439cb1cbd
famesjranko <andrewmcdonald42@gmail.com>
2020-12-10 03:14:12 +1100

    Update temp-report.sh

--- temp-report.sh
+++ temp-report.sh
@@ -15,6 +15,12 @@ date=$(date "+%D - %T")
 locale=Melbourne
 local_data=$(curl wttr.in/$locale?format=%t 2> /dev/null)
 local_temp=$(echo $local_data | tr -dc '[:alnum:]' | sed 's/C$//')
+na=0
+
+if [ "$local_temp" = "SorrywearerunningoutofqueriestotheweatherserviceatthemomentHereistheweatherreportforthedefaultcityjusttoshowyouwhatitlookslikeWewillgetnewqueriesassoonaspossibleYoucanfollowhttpstwittercomigorchubinfortheupdates" ]; then
+    local_temp="--"
+    na=1
+fi
 
 ## get hw temp
 cpu_temp=$(sensors | grep 'Package id 0' | cut -c 17-18)
@@ -22,8 +28,19 @@ cpu_temp=$(sensors | grep 'Package id 0' | cut -c 17-18)
 gpu_temp=$(nvidia-smi -q -d temperature | grep 'GPU Current Temp' | cut -c 45-46)
 
 ## calc hw/local temp delta (can assume local temp will always be lower than hw temp)
-cpu_delta=$(($cpu_temp - $local_temp))
-gpu_delta=$(($gpu_temp - $local_temp))
+if [ $na -eq 1 ]; then
+    cpu_delta="--"
+    gpu_delta="--"
+else
+    cpu_delta=$(($cpu_temp - $local_temp))
+    gpu_delta=$(($gpu_temp - $local_temp))
+fi
+
+## adds leading space to var when local -lt 10'C for ouput alignment
+## eg: transforms (9) into ( 9)
+if [ ${#local_temp} -lt 2 ]; then
+    local_temp=" $local_temp"
+fi
 
 ## print
 echo "$date ($local_temp'C): cpu: $cpu_temp'C (delta: +$cpu_delta'C)"
