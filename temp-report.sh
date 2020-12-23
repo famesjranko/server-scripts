b231c922a1d8d1bd94c28fa54ff1120261e1d7dd
famesjranko <andrewmcdonald42@gmail.com>
2020-12-24 04:43:44 +1100

    Update temp-report.sh

--- temp-report.sh
+++ temp-report.sh
@@ -8,36 +8,24 @@
 #           sensors for cpu temp.
 #=========================================================================
 
+## set wttr.in location variable
+locale=Melbourne
+
 ## get sys date
 date=$(date "+%D - %T")
 
-## set wttr n/a conditional
-na=0
-
 ## get local temp
-locale=Melbourne
 local_data=$(curl wttr.in/$locale?format=%t 2> /dev/null)
 
-## check wttr output against regex, convert if passes
-if ! [[ $local_data =~ [+][0-9].{2}C ]]; then
+## get curl exit code and set wttr n/a conditional
+exit_code=$?
+na=0
+
+## check wttr exit code and output against regex, remove all non-integers if passes.
+if ! [[ $local_data =~ [+][0-9].{2}C ]] || [ $error_code -ne 0 ]; then
     na=1
 else
     local_temp=$(echo $local_data | tr -dc '[:alnum:]' | sed 's/C$//' 2> /dev/null)
-fi
-
-## check converted wttr data sanity
-if [ "$local_temp" = "SorrywearerunningoutofqueriestotheweatherserviceatthemomentHereistheweatherreportforthedefaultcityjusttoshowyouwhatitlookslikeWewillgetnewqueriesassoonaspossibleYoucanfollowhttpstwittercomigorchubinfortheupdates" ]; then
-    local_temp="--"
-    na=1
-elif [ "$local_temp" = "Unknownlocationpleasetry3781421751449631608" ]; then
-    local_temp="--"
-    na=1
-elif [ "$local_temp" = "- " ]; then
-    local_temp="--"
-    na=1
-elif [ -z "$local_temp" ]; then
-    local_temp="--"
-    na=1
 fi
 
 ## get hw temp
@@ -46,7 +34,7 @@ cpu_temp=$(sensors | grep 'Package id 0' | cut -c 17-18)
 gpu_temp=$(nvidia-smi -q -d temperature | grep 'GPU Current Temp' | cut -c 45-46)
 
 ## calc hw/local temp delta (can assume local temp will always be lower than hw temp)
-## returns -- when local temp is not available
+## returns "--" for local temp when na=1
 if [ $na -eq 1 ]; then
     cpu_delta="--"
     gpu_delta="--"
@@ -55,7 +43,7 @@ else
     gpu_delta=$(($gpu_temp - $local_temp))
 fi
 
-## adds leading space to local temp when -lt 10'C for ouput alignment
+## add leading space to local temp when less than 10 for ouput alignment
 ## eg: transforms (9'C) into ( 9'C)
 if [ ${#local_temp} -lt 2 ]; then
     local_temp=" $local_temp"
