bb03e8863c5b3b78bf3e6628cabec3629e01fd65
Andrew McDonald <andrewmcdonald42@gmail.com>
2023-01-25 10:25:39 +1100

    Update srjj-startup.sh

--- docker/scripts/startups/srjj-startup.sh
+++ docker/scripts/startups/srjj-startup.sh
@@ -3,141 +3,107 @@
 ## =================================================================
 ## DOCKER CONTAINER GROUP WAIT FOR REQUIRED CONTAINER START SCRIPT
 ## 
-## This script is used to start a set of containers in a specific
-## order and only when certain conditions are met.
+## Starts a set of containers only if a required container is already
+## running... 
 ##
-## The script will loop indefinitely until the deluge container
-## is running. Once the deluge container is running, it will start
-## the jackett, radarr, sonarr and jellyseerr containers.
-## 
-## If any of the jackett, radarr, sonarr and jellyseerr containers
-## are already running when the script is started, it will log that
-## they are already running and skip starting them.
+## Once the required container is running, it will initialise the
+## container startup group's startup process.
+##
+## The script will loop for ~10mins or all containers are running.
 ## =================================================================
 
 script_name="S-R-J-J STARTUP"
 
-# required container before starting group
-required=rutorrent #deluge
+# set required container
+required=rutorrent
 
-# container start group
-container1=jackett
-container2=radarr
-container3=sonarr
-container4=jellyseerr
+# set container start group
+container_group=(sonarr radarr jackett jellyseerr)
 
-# set initial container states
+# check if required container is running
 required_state=$(docker inspect --format='{{.State.Running}}' $required)
-container1_state=$(docker inspect --format='{{.State.Running}}' $container1)
-container2_state=$(docker inspect --format='{{.State.Running}}' $container2)
-container3_state=$(docker inspect --format='{{.State.Running}}' $container3)
-container4_state=$(docker inspect --format='{{.State.Running}}' $container4)
-
-# check for already running containers
-if [[ "$container1_state" == "true" ]]; then
-    echo $(date +"%y-%m-%d %T")" ["$script_name"]: "$container1" already up!"
+required_running_logged=false
+if [[ "$required_state" == "true" ]]; then
+	echo $(date +"%y-%m-%d %T")" ["$script_name"]: "$required" is running!"
+	required_running_logged=true
 fi
 
-if [[ "$container2_state" == "true" ]]; then
-    echo $(date +"%y-%m-%d %T")" ["$script_name"]: "$container2" already up!"
-fi
+# check for already running containers in startup group
+for container in ${container_group[@]}; do
+    container_state=$(docker inspect --format='{{.State.Running}}' $container)
+    if [[ "$container_state" == "true" ]]; then
+        echo $(date +"%y-%m-%d %T")" ["$script_name"]: "$container" already up!"
+        already_running[$container]=true  # set a flag for container already running
+    else
+        already_running[$container]=false
+    fi
+done
 
-if [[ "$container3_state" == "true" ]]; then
-    echo $(date +"%y-%m-%d %T")" ["$script_name"]: "$container3" already up!"
-fi
-
-if [[ "$container4_state" == "true" ]]; then
-    echo $(date +"%y-%m-%d %T")" ["$script_name"]: "$container4" already up!"
-fi
-
-# init loop counter and echo flag
 loop_count=0
-required_state_up=false
 
+# init startup loop
 while true; do
     (( loop_count++ ))
 
     # check all container states on each loop
     required_state=$(docker inspect --format='{{.State.Running}}' $required)
-    container1_state=$(docker inspect --format='{{.State.Running}}' $container1)
-    container2_state=$(docker inspect --format='{{.State.Running}}' $container2)
-    container3_state=$(docker inspect --format='{{.State.Running}}' $container3)
-    container4_state=$(docker inspect --format='{{.State.Running}}' $container4)
+    for container in ${container_group[@]}; do
+        container_state=$(docker inspect --format='{{.State.Running}}' $container)
+    done
 
-    # start container1 container2 container3 and container4 if required is running
+    # start container_group if required is running
     # if required container is running, that means torrent mapped drive and network are up!
     if [[ "$required_state" == "true" ]]; then
 
         # log required container running status
-        if [[ "$required_state_up" == "false" ]]; then
+        if [[ "$required_running_logged" == "false" ]]; then
             echo $(date +"%y-%m-%d %T")" ["$script_name"]: "$required" is running!"
-            required_state_up=true
+            required_running_logged=true
         fi
 
-        ## CONTAINER1 START SECTION
-        if [[ "$container1_state" == "false" ]]; then
-            docker start $container1 >& /dev/null
-            if [[ $? -eq 0 ]]; then
-                echo $(date +"%y-%m-%d %T")" ["$script_name"]: Starting "$container1"..."
-            else
-                echo $(date +"%y-%m-%d %T")" ["$script_name"]: Error starting "$container1"! Will try again..."
-            fi
-        else
-            echo $(date +"%y-%m-%d %T")" ["$script_name"]: "$container1" started successfully!"
-        fi
+        ## CONTAINER START SECTION
+		for container in ${container_group[@]}; do
+			if [[ "${already_running[$container]}" == "false" ]]; then
+				container_state=$(docker inspect --format='{{.State.Running}}' $container)  # re-assign container state
+				if [[ "$container_state" == "false" ]]; then
+					docker start $container >& /dev/null
+					if [[ $? -eq 0 ]]; then
+						echo $(date +"%y-%m-%d %T")" ["$script_name"]: Starting "$container"..."
+					else
+						echo $(date +"%y-%m-%d %T")" ["$script_name"]: Error starting "$container"! Will try again..."
+					fi
+				else
+					echo $(date +"%y-%m-%d %T")" ["$script_name"]: "$container" started successfully!"
+				fi
+			fi
+		done
 
-        ## CONTAINER2 START SECTION
-        if [[ "$container2_state" == "false" ]]; then
-            docker start $container2 >& /dev/null
-            if [[ $? -eq 0 ]]; then
-                echo $(date +"%y-%m-%d %T")" ["$script_name"]: Starting "$container2"..."
-            else
-                echo $(date +"%y-%m-%d %T")" ["$script_name"]: Error starting "$container2"! Will try again..."
-            fi
-        else
-            echo $(date +"%y-%m-%d %T")" ["$script_name"]: "$container2" started successfully!"
-        fi
+		# exit if all containers are up
+		all_container_group_up=true
+		for container in ${container_group[@]}; do
+			container_state=$(docker inspect --format='{{.State.Running}}' $container)
+			if [[ "$container_state" != "true" ]]; then
+				all_container_group_up=false
+				break
+			fi
+		done
 
-        ## CONTAINER3 START SECTION
-        if [[ "$container3_state" == "false" ]]; then
-            docker start $container3 >& /dev/null
-            if [[ $? -eq 0 ]]; then
-                echo $(date +"%y-%m-%d %T")" ["$script_name"]: Starting "$container3"..."
-            else
-                echo $(date +"%y-%m-%d %T")" ["$script_name"]: Error starting "$container3"! Will try again..."
-            fi
-        else
-            echo $(date +"%y-%m-%d %T")" ["$script_name"]: "$container3" started successfully!"
-        fi
+		if [[ "$all_container_group_up" == "true" ]]; then
+			echo $(date +"%y-%m-%d %T")" ["$script_name"]: Exiting... All containers up!"
+			break
+		fi
+	fi
 
-        ## CONTAINER4 START SECTION
-        if [[ "$container4_state" == "false" ]]; then
-            docker start $container4 >& /dev/null
-            if [[ $? -eq 0 ]]; then
-                echo $(date +"%y-%m-%d %T")" ["$script_name"]: Starting "$container4"..."
-            else
-                echo $(date +"%y-%m-%d %T")" ["$script_name"]: Error starting "$container4"! Will try again..."
-            fi
-        else
-            echo $(date +"%y-%m-%d %T")" ["$script_name"]: "$container4" started successfully!"
-        fi
-
-    else
-        echo $(date +"%y-%m-%d %T")" ["$script_name"]: "$required" is not running yet..."
-    fi
-
-    # exit if all containers are up
-    if [[ "$container1_state" == "true" && "$container2_state" == "true" && "$container3_state" == "true" && "$container4_state" == "true" ]]; then
-        echo $(date +"%y-%m-%d %T")" ["$script_name"]: Exiting... All containers up!"
-        break
-    fi
-
-    # exit if more than 10mins elapsed and some/all containers won't start
-    if [[ $loop_count -gt 60 ]]; then
-         echo $(date +"%y-%m-%d %T")" ["$script_name"]: Exiting after 60 loops... "$container1"="$container3_state","$container2"="$container2_state","$container3"="$container3_state","$container4"="$container4_state
-        break
-    fi
+	# exit if more than 10mins elapsed and some/all container_group won't start
+	if [[ $loop_count -gt 60 ]]; then
+		echo -n $(date +"%y-%m-%d %T")" ["$script_name"]: Exiting after 60 loops... "
+		for container in ${container_group[@]}; do
+			container_state=$(docker inspect --format='{{.State.Running}}' $container)
+			echo -n $container"="$container_state","
+		done
+		echo
+		break
+	fi
 
     sleep 10
 done
-
