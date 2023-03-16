115b443bf0d55585eb30900a6ee8352ca5dbe3a6
famesjranko <andrewmcdonald42@gmail.com>
2023-03-17 02:43:37 +1100

    Update dpinger-monitor.sh

--- pfsense/dpinger-monitor.sh
+++ pfsense/dpinger-monitor.sh
@@ -2,12 +2,21 @@
 
 # ===========================
 # Author: Andrew J. McDonald
-# Date: 2023-03-16
+# Date: 2023-03-15
 # ===================================================================================
 # This script was written for pfsense, it checks if the dpinger service is running
 # and restarts it if necessary. If dpinger is down, it will attempt to restart it up
 # to 5 times, with a 5 second sleep period between attempts.
 # ===================================================================================
+
+# Define a function to check if dpinger is running
+check_dpinger() {
+  if pgrep dpinger > /dev/null; then
+    return 0
+  else
+    return 1
+  fi
+}
 
 # Define a function to start dpinger and retry if necessary
 start_dpinger() {
@@ -16,20 +25,23 @@ start_dpinger() {
 
   # Set the maximum number of attempts and the sleep period between attempts
   max_attempts=5
-  attempt=1
   sleep_period=5
+  
+  attempt=1
 
   # Loop through up to the maximum number of attempts to restart dpinger
   while [ $attempt -le $max_attempts ]; do
-    # Check if dpinger is now running and return success if it is
-    if /usr/local/sbin/pfSsh.php playback svc status dpinger | grep 'running' > /dev/null 2>&1; then
-      return 0
-    # If dpinger is still not running, log the attempt number and sleep before trying again
-    else
+    # If dpinger is not running, log the attempt number and sleep before trying again
+    if ! check_dpinger; then
       echo "$(date '+%y-%m-%d %T') [STATUS]: attempt $attempt failed to restart dpinger..."
       attempt=$((attempt + 1))
       sleep $sleep_period
-      start_dpinger
+
+      # try restarting dpinger again...
+      /usr/local/sbin/pfSsh.php playback svc start dpinger > /dev/null 2>&1
+    # return success if it is
+    else
+      return 0
     fi
   done
 
@@ -37,19 +49,8 @@ start_dpinger() {
   return 1
 }
 
-# Define a function to check if dpinger is running
-check_dpinger() {
-  if /usr/local/sbin/pfSsh.php playback svc status dpinger | grep 'running' > /dev/null 2>&1; then
-    return 0
-  else
-    return 1
-  fi
-}
-
 # Check if dpinger is running
-if check_dpinger; then
-  echo "$(date '+%y-%m-%d %T') [STATUS]: dpinger is up!"
-else
+if ! check_dpinger; then
   # If dpinger is down, attempt to restart it and log the success or failure
   echo "$(date '+%y-%m-%d %T') [STATUS]: dpinger is down!"
   if start_dpinger; then
