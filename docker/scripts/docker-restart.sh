15be344c339b6aa6632ab7ce9710821fa90a152c
famesjranko <andrewmcdonald42@gmail.com>
2023-03-22 04:51:05 +1100

    Update docker-restart.sh

--- docker/scripts/docker-restart.sh
+++ docker/scripts/docker-restart.sh
@@ -1,55 +1,49 @@
 #!/bin/bash
 
-## ===========================================
-## simple stop/restart docker container script
-## ===========================================
+# ===========================
+# Author: Andrew J. McDonald
+# Date: 2023-03-22
+# ============================================================================
+# Description: simple script to stop and restart all and only currently
+# running docker containers
+# ============================================================================
 
-echo "currently running docker containers: "
-docker ps | awk '{ print $2}' | cut -c 13-
+# Get list of currently running containers
+echo -n "Currently running docker containers: "
+containers_to_restart=$(docker ps --format "{{.Names}}" | sort)
 
-echo
-printf "stopping containers"
+# check if there are any running containers
+if [ -z "$containers_to_restart" ]; then
+    echo "No running containers found."
+    exit 1
+fi
 
-# stop running containers
-#docker stop $(docker ps -a -q) > /dev/null 2>&1 &
-#docker stop $(docker ps -a -q) &
+# Convert the list to an array
+readarray -t containers_to_restart <<< "$containers_to_restart"
 
-for (( i=1; i<=3; i++ ))
-do
-	sleep .2
-	printf ". "
+# Print the list of containers to restart
+echo -e "${containers_to_restart[@]}"
+
+# Stop running containers
+echo -e "\nStopping containers"
+if ! docker stop "${containers_to_restart[@]}"; then
+    echo "Failed to stop containers."
+    exit 1
+fi
+
+# Print 5 second counter...
+echo -ne "\nWaiting for 5 seconds: "
+for (( i=1; i<=5; i++ )); do
+  echo -n " $i"
+  sleep 1
 done
 
-echo
-docker stop $(docker ps -a -q)
+# Restart previously stopped containers
+echo -e "\n\nRestarting containers"
+if ! docker start "${containers_to_restart[@]}"; then
+    echo "Failed to restart containers."
+    exit 1
+fi
 
-echo
-printf "wait 5 seconds: "
-
-for (( i=1; i<=5; i++ ))
-do
-	sleep 1
-	#echo "$i"
-	printf " $i"
-done
-sleep 1
-
-echo && echo
-printf "restarting containers"
-
-# restart previously stopped containers
-#docker start $(docker ps -a -q -f status=exited) > /dev/null 2>&1 &
-#docker start $(docker ps -a -q -f status=exited) &
-
-for (( i=1; i<=3; i++ ))
-do
-        sleep .2
-        printf ". "
-done
-echo
-docker start $(docker ps -a -q -f status=exited)
-
-echo
-echo "finished!"
-
+echo -e "\nFinished!"
 exit 0
