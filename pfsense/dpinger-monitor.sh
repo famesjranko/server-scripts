6b8a50eb227ae82ff569e30b46602b0ffcce97ce
famesjranko <andrewmcdonald42@gmail.com>
2023-03-22 03:17:48 +1100

    Update dpinger-monitor.sh

--- pfsense/dpinger-monitor.sh
+++ pfsense/dpinger-monitor.sh
@@ -2,61 +2,79 @@
 
 # ===========================
 # Author: Andrew J. McDonald
-# Date: 2023-03-15
+# Date: 2023-03-22
 # ===================================================================================
 # This script was written for pfsense, it checks if the dpinger service is running
 # and restarts it if necessary. If dpinger is down, it will attempt to restart it up
-# to 5 times, with a 5 second sleep period between attempts.
+# to n times, with a n second sleep period between attempts.
 # ===================================================================================
 
-# Define a function to check if dpinger is running
-check_dpinger() {
-  #if pgrep dpinger > /dev/null; then # <--- causes issue when used with cron during testing 
+# Set whether to run once or loop continuously...
+SINGLE_RUN=true
+
+# Function to check if dpinger is not running
+# returns 0 if dpinger is down
+# returns 1 if dpinger is up
+dpinger_down() {
   if /usr/local/sbin/pfSsh.php playback svc status dpinger | grep "Service dpinger is running." > /dev/null 2>&1; then
-    return 0
-  else
     return 1
+  else
+    return 0
   fi
 }
 
-# Define a function to start dpinger and retry if necessary
-start_dpinger() {
-  echo "$(date '+%y-%m-%d %T') [STATUS]: restarting dpinger..."
-  /usr/local/sbin/pfSsh.php playback svc start dpinger > /dev/null 2>&1
+# Function to start dpinger and retry if necessary
+# returns 0 if dpinger restart succeeds
+# returns 1 if dpinger restart fails
+dpinger_start() {
+  # Set max attempts
+  max_attempts=10
 
-  # Set the maximum number of attempts and the sleep period between attempts
-  max_attempts=5
-  sleep_period=5
-  
+  # Set wait before next attempt
+  sleep_period=15
+
+  echo "$(date '+%y-%m-%d %T') [STATUS]: restarting dpinger..."
+
   attempt=1
 
-  # Loop through up to the maximum number of attempts to restart dpinger
+  # Attempt to restart dpinger
   while [ $attempt -le $max_attempts ]; do
-    # If dpinger is not running, log the attempt number and sleep before trying again
-    if ! check_dpinger; then
+    # restart dpinger...
+    /usr/local/sbin/pfSsh.php playback svc start dpinger > /dev/null 2>&1
+
+    if dpinger_down; then
       echo "$(date '+%y-%m-%d %T') [STATUS]: attempt $attempt failed to restart dpinger..."
       attempt=$((attempt + 1))
       sleep $sleep_period
-
-      # try restarting dpinger again...
-      /usr/local/sbin/pfSsh.php playback svc start dpinger > /dev/null 2>&1
-    # return success if it is
     else
       return 0
     fi
   done
 
-  # If all attempts fail, return failure
-  return 1
+  # Run final dpinger status check
+  if dpinger_down; then
+    return 1
+  else
+    return 0
+  fi
 }
 
-# Check if dpinger is running
-if ! check_dpinger; then
-  # If dpinger is down, attempt to restart it and log the success or failure
-  echo "$(date '+%y-%m-%d %T') [STATUS]: dpinger is down!"
-  if start_dpinger; then
-    echo "$(date '+%y-%m-%d %T') [SUCCESS]: dpinger restarted!"
-  else
-    echo "$(date '+%y-%m-%d %T') [ERROR]: all attempts failed to restart dpinger."
+while true; do
+  if dpinger_down; then
+    echo "$(date '+%y-%m-%d %T') [STATUS]: dpinger is down!"
+    if dpinger_start; then
+      echo "$(date '+%y-%m-%d %T') [SUCCESS]: dpinger restarted!"
+    else
+      echo "$(date '+%y-%m-%d %T') [ERROR]: all attempts failed to restart dpinger."
+    fi
+  #else
+  #  echo "$(date '+%y-%m-%d %T') [STATUS]: dpinger is up."
   fi
-fi
+
+  if $SINGLE_RUN; then
+    break
+  fi
+
+  # wait 5mins before checking again...
+  sleep 300
+done
