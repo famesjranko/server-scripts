0011efc483fec1b8173ac415141a7395016b7337
famesjranko <andrewmcdonald42@gmail.com>
2023-03-22 04:53:12 +1100

    Update torrent-startup.sh

--- docker/scripts/startups/torrent-startup.sh
+++ docker/scripts/startups/torrent-startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# ===========================
+# Author: Andrew J. McDonald
+# Date: 2023-03-22
 ## =====================================================================
 ## DOCKER CONTAINER WAIT FOR NETWORK/MOUNT POINT START SCRIPT
 ##
